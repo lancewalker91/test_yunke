75cda4b784a6263886b7bbd84ed84472fb3d7d16
litao <litao15982@talkweb.com.cn>
2018-03-20 11:14:05 +0800

    test

--- jenkins_test_yunke.bat
+++ jenkins_test_yunke.bat
@@ -1,1 +1,1 @@
-python runtest.py >> report/%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%log.txt 2>&1
+python runtest.py >> C:\Users\Administrator\Desktop\test_yunke\report\%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%log.txt 2>&1
