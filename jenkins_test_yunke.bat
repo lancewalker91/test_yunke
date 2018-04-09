abd9897b4b2e3d43b9b85be90c11c1e7942ac014
litao <litao15982@talkweb.com.cn>
2018-04-09 11:33:47 +0800

    update

---
+++ jenkins_test_yunke.bat
@@ -0,0 +1,1 @@
+python C:\Users\Administrator\Desktop\test_yunke\runtest.py >> C:\Users\Administrator\Desktop\test_yunke\report\%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%log.txt 2>&1
